474564b6c19612e5a9bd8e4cafc438bb78fe3db2
RudekAlone <44509433+RudekAlone@users.noreply.github.com>
2025-11-05 13:00:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const films = [
+  {
+    title: "Zielona mila",
+    originalTitle: "The Green Mile",
+    year: 1999,
+    genre: "Dramat",
+    rating: 8.6,
+    image: "https://fwcdn.pl/fpo/08/62/862/7517878_1.10.webp",
+  },
+  {
+    title: "Incepcja",
+    originalTitle: "Inception",
+    year: 2010,
+    genre: "Sci-Fi",
+    rating: 8.2,
+    image: "https://fwcdn.pl/fpo/08/91/500891/7354571_1.10.webp",
+  }
+];
+
